d91b210a2ca1d058c2695e26943c297ca2241a6b
fewizz <hazeevaidar@gmail.com>
2019-04-24 19:46:11 +0300

    customize freetype build

--- lib/freetype/build.sh
+++ lib/freetype/build.sh
@@ -1,3 +1,33 @@
+mkdir -p build
+
 cd build
-cmake -DCMAKE_C_COMPILER=$CC ../git
-cmake --build .
+
+#cmake -DCMAKE_C_COMPILER=$CC ../git
+#cmake --build .
+
+src_files0=\
+'base/ftsystem '\
+'base/ftinit '\
+'base/ftdebug '\
+'base/ftbase '\
+\
+'base/ftbbox '\
+'base/ftglyph '\
+'base/ftbitmap '\
+\
+'sfnt/sfnt '\
+'truetype/truetype '\
+\
+'raster/raster '\
+'smooth/smooth '\
+\
+'psnames/psnames '
+
+for x in ${src_files0}; do src_files="${src_files} ../git/src/${x}.c"; done
+
+${CC} -c \
+	${src_files} \
+	-I../git/include \
+	-DFT2_BUILD_LIBRARY
+
+ar -rs libfreetype.a `find *.o` 
